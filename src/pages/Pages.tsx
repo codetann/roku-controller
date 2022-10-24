96c7faafd38add8b3fcfe3b6d0faf76b37bd051d
Tanner Thomas <thetannerthomas@gmail.com>
2022-10-23 22:54:06 -0600

    add landing page

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { LandingPage } from "./landing-page";
 
 function Pages() {
   const location = useLocation();
@@ -7,7 +8,7 @@ function Pages() {
   return (
     <AnimatePresence exitBeforeEnter>
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<div />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </AnimatePresence>
   );
---
+++ src/pages/landing-page/LandingPage.tsx
@@ -0,0 +1,11 @@
+import { FadeIn } from "@/animations";
+
+function LandingPage() {
+  return (
+    <FadeIn>
+      <h1>Hello World</h1>
+    </FadeIn>
+  );
+}
+
+export { LandingPage };
